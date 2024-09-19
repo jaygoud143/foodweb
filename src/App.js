70946dbecbf856d4e004c860d1a354d74106ba2c
jaygoud143 <jaygoud524@gmail.com>
2024-09-19 11:56:47 -0500

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './components/styles/App.css';
+import Header from "./components/ui/Header.js";
+
+function App() {
+  return (
+    <div>
+         <Header />
+    </div>
+   
+  
+)
+}
+
+export default App;
